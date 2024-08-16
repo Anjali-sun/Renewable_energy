b0167635a246712a1dc51f789ac093b562478fd8
Anjali-sun <anjuvipin@gmail.com>
2024-08-16 10:49:28 +0800

    Update main.py

--- main.py
+++ main.py
@@ -18,10 +18,14 @@ import pandas as pd
 
 #od.download("https://www.kaggle.com/datasets/jamesvandenberg/renewable-power-generation/")
 od.download("https://www.kaggle.com/datasets/belayethossainds/renewable-energy-world-wide-19652022")
-file1=("renewable-power-generation/top20CountriesPowerGeneration.csv")
-file2=("renewable-power-generation/renewablesTotalPowerGeneration.csv")
-newdata1=pd.read_csv(file1)
-newdata2=pd.read_csv(file2)
-print("printing TOP20 Countries Power Generation")
-print(newdata1)
-print(newdata2)
+#file1=("renewable-power-generation/top20CountriesPowerGeneration.csv")
+#file2=("renewable-power-generation/renewablesTotalPowerGeneration.csv")
+#newdata1=pd.read_csv(file1)
+#newdata2=pd.read_csv(file2)
+#print("printing TOP20 Countries Power Generation")
+#print(newdata1)
+#print(newdata2)
+file='renewable-energy-world-wide-19652022/renewable-share-energy.csv'
+print (file)
+newdata3=pd.read_csv(file1)
+newdata3
